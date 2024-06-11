39d4e10dd4b77cd0a565a1056235f1dbc438ecd9
Tatak Galih <tatakgalih@yahoo.com>
2024-06-11 20:00:08 +0700

    add integrasi report sop

---
+++ pages/dashboard/report-sop/anak-kandang/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ASYNC_KEY } from "~/constants/api";
+
+definePageMeta({
+  layout: "dashboard",
+  middleware: ["dashboard"],
+});
+useSeoMeta({
+  title: "Report SOP Harian | Layer Apps",
+  description: "Report SOP Harian | Layer Apps",
+});
+const { getAllSopsByRoleId } = useFetchSOP();
+const { data } = await useAsyncData(ASYNC_KEY.SOP_ANAK_KANDANG, async () =>
+  getAllSopsByRoleId(4)
+);
+</script>
+
+<template>
+  <div class="content-wrapper-height pb-10 pt-[14px] bg-white">
+    <SOPTemplate :sop-items="data || []" />
+  </div>
+</template>
